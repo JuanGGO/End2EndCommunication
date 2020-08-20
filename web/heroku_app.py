590107f41e03c1f392766c8d01023a79d15ad374
juan <jggarciao2015@gmail.com>
2020-08-19 20:26:03 -0700

    all the stuff

--- web/heroku_app.py
+++ web/heroku_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1>Hello World! Heroku<h1>'
+
+
+@app.route('/from_flutter', methods=['POST'])
+def get_signal_from_flutter(json):
+    param = request.form.get('title')
+    print(param)
 
 
 if __name__ == '__main__':
